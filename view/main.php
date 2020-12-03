bbcf7511d19c572df2ad2192f4db2db534ff87af
jhang0912 <fueqaxvk@gmail.com>
2020-12-04 02:58:08 +0800

    開始製作外觀核心組件

---
+++ view/main.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <link rel="stylesheet" href="main.css">
+    <title>Document</title>
+  <style>
+  </style>
+</head>
+<body>
+  <section id="header" class=" container-fluid row p-0 m-0">
+    <div class="left col-sm-12 col-md-12 col-lg-6">AAA</div>
+    <div class="right col-sm-12 col-md-12 col-lg-6">BBB</div>
+    <div class="bottom col-12">CCC</div>
+  </section>
+
+  <section id="left" class="col-12 col-sm-12 col-md-2 p-0">
+    <div class="top1 container col-12">DDD</div>
+    <div class="top2 container col-12">
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>4</div>
+      <div>5</div>
+      <div>6</div>
+      <div>7</div>
+      <div>8</div>
+      <div>9</div>
+      <div>10</div>
+    </div>
+  </section>
+
+  <section id="article" class="col-12 col-sm-12 col-md-10 p-0">
+    <div class=""></div>
+  </section>
+
+  <section id="base">
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+
+  </section>
+</body>
+</html>
